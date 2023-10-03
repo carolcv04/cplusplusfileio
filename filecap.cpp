31229dacbd532dc4c1c02f85e4df0d983ed60137
carolcv04 <caraballovlez@chapman.edu>
2023-10-02 22:05:22 -0700

    Add existing file

--- filecap.cpp
+++ filecap.cpp
@@ -8,14 +8,41 @@ int main (int argc, char*argv[]){
     file = argv[1];
 
     try{
-        fstream text;
-        text.open('"' + file + '"', ios::in);
+        ofstream fout;
+        ifstream fin;
+        fin.open("text.txt", ios::in);
 
-        if(!text){
+        if(!fin){
             throw string("File does not exist.\n");
         }
+        else {
+            char x;
+            fout.open(file, ios::out);
+            while (getline(fin, line)) {
+                fout << endl;
+                for(int i=0; i < line.size(); i++){
+                    x = line[i];
+
+                    if (isupper(x)){
+                        x = tolower(x);
+                        fout << x;
+                    } else if (islower(x)) {
+                        x = toupper(x);
+                        fout << x;
+                    } else if (!isalpha(x)) {
+                        fout << x;
+                    } else {
+                        fout << " ";
+                    }
+                }
+
+            }
+            fout.close(); 
+        }
+        fin.close();
     }
     catch (string error){
         cout << error;
     }
+    return 0;
 }
